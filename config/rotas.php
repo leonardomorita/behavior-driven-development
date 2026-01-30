02a4982254a617f235b9459a46ec0420487980ad
leonardomorita <leonardomorita2@gmail.com>
2026-01-30 01:15:46 -0300

    ✨ Aula 6 - Testando antes

--- config/rotas.php
+++ config/rotas.php
@@ -1,15 +1,18 @@
 <?php
 
+use Alura\Armazenamento\Controller;
+
 return [
-    '/login' => \Alura\Armazenamento\Controller\FormularioLogin::class,
-    '/fazer-login' => \Alura\Armazenamento\Controller\Login::class,
-    '/logout' => \Alura\Armazenamento\Controller\Logout::class,
-    '/novo-curso' => \Alura\Armazenamento\Controller\FormularioInsercaoCurso::class,
-    '/salvar-curso' => \Alura\Armazenamento\Controller\PersistenciaCurso::class,
-    '/listar-cursos' => \Alura\Armazenamento\Controller\ListaDeCursos::class,
-    '/editar-curso' => \Alura\Armazenamento\Controller\FormularioEdicaoCurso::class,
-    '/excluir-curso' => \Alura\Armazenamento\Controller\ExclusaoDeCurso::class,
-    '/nova-formacao' => \Alura\Armazenamento\Controller\FormularioInsercaoFormacao::class,
-    '/salvar-formacao' => \Alura\Armazenamento\Controller\PersistenciaFormacao::class,
-    '/listar-formacoes' => \Alura\Armazenamento\Controller\ListaDeFormacoes::class,
+    '/login' => Controller\FormularioLogin::class,
+    '/fazer-login' => Controller\Login::class,
+    '/logout' => Controller\Logout::class,
+    '/novo-curso' => Controller\FormularioInsercaoCurso::class,
+    '/salvar-curso' => Controller\PersistenciaCurso::class,
+    '/listar-cursos' => Controller\ListaDeCursos::class,
+    '/editar-curso' => Controller\FormularioEdicaoCurso::class,
+    '/excluir-curso' => Controller\ExclusaoDeCurso::class,
+    '/nova-formacao' => Controller\FormularioInsercaoFormacao::class,
+    '/salvar-formacao' => Controller\PersistenciaFormacao::class,
+    '/listar-formacoes' => Controller\ListaDeFormacoes::class,
+    '/excluir-formacao' => Controller\ExclusaoDeFormacao::class,
 ];
